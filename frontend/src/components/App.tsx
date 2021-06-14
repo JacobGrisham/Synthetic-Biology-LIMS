18dfc2b9bdc993ad9af762d2b9adc097c921dbcb
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-06-13 21:38:23 -0700

    :recycle: migrate to typescript and redux
    refactor App.js to use typescript and redux

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,51 @@
+// React
+import React from "react";
+import '../assets/App.css';
+import Home from "./Home"
+import ErrorBoundary from './ErrorBoundary'
+// Redux
+import { AnyAction } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
+import { connect } from 'react-redux';
+import { requestCompanies } from '../redux/actions/actions';
+
+export interface ICompany {
+  id: number;
+  name: string;
+  tier: string;
+}
+
+interface IAppState {
+  compaines: Array<ICompany>;
+  requestCompanies: any;
+}
+
+interface IAppProps {
+  companies: any;
+  isPending: boolean;
+  onRequestOfCompanies?: any;
+}
+
+const mapStateToProps = (state: IAppState) => {
+  return {
+    companies: state.requestCompanies.users,
+    isPending: state.requestCompanies.isPending,
+    error: state.requestCompanies.error
+  }
+}
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<IAppState, void, AnyAction>) => {
+  return {
+    onRequestOfUsers: () => dispatch(requestCompanies())
+  }
+}
+
+const App = (props: IAppProps) => {
+  return (
+    <ErrorBoundary>
+      <Home {...props}/>
+    </ErrorBoundary>
+  );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
