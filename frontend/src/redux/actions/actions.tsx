932e8a26528243e0476c7e34cf7bb8f89738375b
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-06-13 21:28:12 -0700

    :recycle: migrate to redux
    add redux directory to store redux files. add actions.tsx and reducers.tsx.

---
+++ frontend/src/redux/actions/actions.tsx
@@ -0,0 +1,20 @@
+import { Dispatch } from 'redux';
+import { apiCall } from '../../api/api';
+import { API_URL } from '../../constants/constants'
+import { 
+  REQUEST_COMPANIES_PENDING, 
+  REQUEST_COMPANIES_SUCCESS, 
+  REQUEST_COMPANIES_FAILED
+} from '../../constants/constants.js';
+
+
+// This is a higher order function: a function that returns a function
+export const requestCompanies = (apiURL = API_URL) => async (dispatch: Dispatch) => {
+  try {
+    dispatch({ type: REQUEST_COMPANIES_PENDING })
+    const data = await apiCall(apiURL)
+    dispatch({ type: REQUEST_COMPANIES_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({ type: REQUEST_COMPANIES_FAILED, payload: error })
+  }
+}
---
+++ frontend/src/redux/reducers/reducers.tsx
@@ -0,0 +1,28 @@
+import { 
+  REQUEST_COMPANIES_PENDING, 
+  REQUEST_COMPANIES_SUCCESS, 
+  REQUEST_COMPANIES_FAILED
+} from '../../constants/constants.js';
+
+const initialStateCompanies = {
+  isPending: false,
+  compaines: [],
+  error: ''
+}
+
+interface actionObject {
+  [key: string]: any
+}
+
+export const requestCompanies = (state: object = initialStateCompanies, action: actionObject = {}) => {
+  switch(action.type) {
+    case REQUEST_COMPANIES_PENDING:
+      return Object.assign({}, state, { isPending: true })
+    case REQUEST_COMPANIES_SUCCESS:
+      return Object.assign({}, state, { companies: action.payload, isPending: false })
+    case REQUEST_COMPANIES_FAILED:
+      return Object.assign({}, state, { error: action.payload, isPending: false })
+    default:
+      return state
+  }
+}
