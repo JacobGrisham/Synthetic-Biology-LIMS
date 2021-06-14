5e2678a33d0c476c98ca37e868c1f8a66741e6f6
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-06-13 21:41:51 -0700

    :recycle: migrate to typescript
    refactor Header.js to use typescript

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="App">
+      <h3 className="App-header">Companies Using LIMS</h3>
+    </div>
+  );
+}
+
+export default Header;
