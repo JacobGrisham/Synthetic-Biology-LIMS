e7df0e03ea614864d28ce49c0f83d1e7531a1c6f
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-06-13 21:37:13 -0700

    :recycle: migrate to typescript
    refactor index.js to use typescript

---
+++ frontend/src/index.tsx
@@ -0,0 +1,39 @@
+// React
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { requestCompanies } from './redux/reducers/reducers';
+// Redux Middleware
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+// Components
+import App from './components/App';
+
+const logger = createLogger();
+
+const rootRedcuer = combineReducers({ requestCompanies });
+const store = createStore(rootRedcuer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root') as HTMLElement
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
