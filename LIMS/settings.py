0385e12ce14ee069a94c1576e88d6673ad46110d
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-06-13 21:49:22 -0700

    :card_file_box: migrate to PostgreSQL
    update DATABASES in settings.py to use PostgreSQL

--- LIMS/settings.py
+++ LIMS/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'LIMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.postgresql',
+        'USER': 'jacobdavidgrisham',
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
