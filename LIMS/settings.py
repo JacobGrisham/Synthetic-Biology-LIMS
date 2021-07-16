d963b992dd1255f65e660a2c6fc7d3f7c7a1c760
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-07-15 22:24:52 -0700

    :adhesive_bandage: update environment syntax in settings.py
    config (from decouple) wasn't working so swapped it with environ

--- LIMS/settings.py
+++ LIMS/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+import environ
+env = environ.Env()
+env.read_env(env.str('ENV_PATH', './.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -98,9 +100,9 @@ WSGI_APPLICATION = 'LIMS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.postgresql',
+        'NAME': 'db.postgresql',
         'USER': 'jacobdavidgrisham',
-        'PASSWORD': config('DB_PASSWORD'),
+        'PASSWORD': env('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': 5432,
     }
