47bbc7345ec2a5eb3e61b3088f6e3c4e880e97ae
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-07-22 01:10:24 -0700

    :wrench: update settings.py
    update env

--- LIMS/settings.py
+++ LIMS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', 'sample_secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
