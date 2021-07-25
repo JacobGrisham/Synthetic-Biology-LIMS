a36398dce873e7b24097d8cc26eeb9c7033938f7
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-07-25 16:49:03 -0700

    :wrench: update settings.py

--- django-backend/LIMS/settings.py
+++ django-backend/LIMS/settings.py
@@ -135,7 +135,6 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'predictiondb',
-            'USER': 'postgres_user',
             'PASSWORD': 'postgres_password',
             'HOST': '127.0.0.1',
             'PORT': '5432',
